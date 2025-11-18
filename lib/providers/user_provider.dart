75417415f8b32b696dec11d41798f09ac2d93787
VanPhuc-027 <phucvjppromax@gmail.com>
2025-11-18 13:17:56 +0700

    quản lý user

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:learningmanagement/models/user_model.dart';
+import 'package:learningmanagement/providers/auth_provider.dart';
+
+final userProvider = StateNotifierProvider<UserNotifier, List<UserModel>>(
+  (ref) => UserNotifier(),
+);
+
+class UserNotifier extends StateNotifier<List<UserModel>> {
+  UserNotifier() : super([]) {
+    _listenUsers();
+  }
+
+  final _db = FirebaseDatabase.instance.ref("users");
+
+  void _listenUsers() {
+    _db.onValue.listen((event) {
+      final data = event.snapshot.value;
+
+      if (data == null) {
+        state = [];
+        return;
+      }
+
+      final map = Map<String, dynamic>.from(data as Map);
+
+      final users = map.entries.map((e) {
+        return UserModel.fromMap(
+          e.key, // uid
+          Map<String, dynamic>.from(e.value),
+        );
+      }).toList();
+
+      state = users;
+    });
+  }
+
+  /// Kích hoạt / vô hiệu hóa user
+  Future<void> toggleActive(String uid) async {
+    final current = state.firstWhere((u) => u.uid == uid).isActive;
+    await _db.child(uid).update({"isActive": !current});
+  }
+
+  /// Reset mật khẩu (demo)
+  Future<void> resetPassword(String uid) async {
+    await _db.child(uid).update({"password": "123456"});
+  }
+
+  /// Xóa user
+  Future<void> deleteUser(String uid) async {
+    await _db.child(uid).remove();
+  }
+
+  /// Đổi role
+  Future<void> changeRole(String uid, UserRole role) async {
+    await _db.child(uid).update({"role": role.name});
+  }
+}
--- lib/screens/Admin_screens/admin_main_screen.dart
+++ lib/screens/Admin_screens/admin_main_screen.dart
@@ -1,43 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:learningmanagement/models/user_model.dart';
 import 'package:learningmanagement/providers/auth_provider.dart';
+import 'package:learningmanagement/providers/user_provider.dart';
 import 'package:learningmanagement/screens/Admin_screens/UserManagement/user_management.dart';
 import 'admin_home.dart';
 
-class AdminMainScreen extends StatefulWidget {
+class AdminMainScreen extends ConsumerStatefulWidget {
   final List<UserModel> users;
 
   const AdminMainScreen({super.key, required this.users});
 
   @override
-  State<AdminMainScreen> createState() => _AdminMainScreenState();
+  ConsumerState<AdminMainScreen> createState() => _AdminMainScreenState();
 }
 
-class _AdminMainScreenState extends State<AdminMainScreen> {
+
+class _AdminMainScreenState extends ConsumerState<AdminMainScreen> {
   int _currentIndex = 0;
 
   void _onToggleActive(String uid) {
-    // logic ở provider
+   ref.read(userProvider.notifier).toggleActive(uid);
   }
 
   void _onResetPassword(String uid) {
-    // logic ở provider
+    ref.read(userProvider.notifier).resetPassword(uid);
   }
 
   void _onDeleteUser(String uid) {
-    // logic ở provider
+    ref.read(userProvider.notifier).deleteUser(uid);
   }
 
   void _onChangeRole(String uid, UserRole role) {
-    // logic ở provider
+    ref.read(userProvider.notifier).changeRole(uid, role);
   }
 
   @override
   Widget build(BuildContext context) {
+    final users = ref.watch(userProvider);
     final pages = [
       const AdminHome(),
       UserManagementScreen(
-        users: widget.users,
+        users: users,
         onToggleActive: _onToggleActive,
         onResetPassword: _onResetPassword,
         onDeleteUser: _onDeleteUser,
